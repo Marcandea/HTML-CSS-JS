88c75cdb623f19351c58a81b2cfdbaca27a3f180
Marcandea <marcandea2@gmai.com>
2020-01-23 11:52:24 -0400

    delete button created

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var students = [];
 var tbody;
-8
+var table;
+
 function Student(name, lastName, birthday) {
     this.name = name;
     this.lastName = lastName;
@@ -19,12 +20,13 @@ function saving() {
     const birthday = document.getElementById('birthday').value;
     var student = new Student(name, lastName, birthday);
     students.push(student);
-    this.createCellContent(student);
+    this.createRowContent(student);
+    console.log(students)
 };
 
 function createTable(){
     var container = document.getElementById('container');
-    var table = document.createElement('table');
+    table = document.createElement('table');
     tbody = document.createElement('tbody');
     container.appendChild(table);
     table.appendChild(tbody);
@@ -34,51 +36,35 @@ function createTable(){
 function createNameFields(){
     var nameRow = document.createElement('tr');
     for (var item in new Student){
-        var nameCell = document.createElement('td');
+        var nameCell = document.createElement('th');
         nameCell.textContent = item.toUpperCase();
         nameRow.appendChild(nameCell);
     }
     tbody.appendChild(nameRow); 
 };
 
-function createCellContent(vals){
+function createRowContent(student){
     var row = document.createElement('tr');
-    for (var item in vals){
+    for (var item in student){
         var cell = document.createElement('td');
-        cell.textContent = vals[item];
+        cell.textContent = student[item];
         row.appendChild(cell);
     }
-    var cellDelete = document.createElement('td');
-    cellDelete.textContent = " X "
-    row.appendChild(cellDelete);
+    row.appendChild(this.createDeleteButton());
     tbody.appendChild(row);
 };
 
-// function draw() {
-//     var container = document.getElementById('container');
-//     var table = document.createElement('table');
-//     var tbody = document.createElement('tbody');
-//     // loop array
-//     for (i = 0; i < students.length; i++) {
-//         // get inner array
-//         var vals = students[i];
-//         // create tr element
-//         var row = document.createElement('tr');
-//         // loop inner array
-//         for (var b = 0; b < 3; b++) {
-//             // create td element
-//             var cell = document.createElement('td');
-//             // set text
-//             cell.textContent = vals.name;
-//             console.log(vals.name);
-//             // append td to tr
-//             row.appendChild(cell);
-//         }
-//         //append tr to tbody
-//         tbody.appendChild(row);
-//     }
-//     // append tbody to table
-//     table.appendChild(tbody);
-//     // append table to container
-//     container.appendChild(table);
-// }
+function createDeleteButton(){
+    var cellDelete = document.createElement('td');
+    var button = document.createElement('button');
+    button.type = 'button';
+    button.innerText = ' X ';
+    button.setAttribute('onclick', "deleteStudent("+1+");")
+    cellDelete.appendChild(button);
+    return cellDelete;
+};
+
+function deleteStudent(position){
+    table.deleteRow(position);
+    students.pop(position);
+};
