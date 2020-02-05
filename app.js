9a42ee8a7d91e1d279bce3a9df24b9bf49c02737
Marcandea <marcandea2@gmai.com>
2020-02-04 23:12:29 -0400

    form and table add and delete finished

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function Student(name, lastName, birthday) {
     this.lastName = lastName;
     this.birthday = birthday;
 
-    function getInfo () {
+    function getInfo() {
         console.log(this.name + " " + this.lastName);
     }
 };
@@ -24,28 +24,29 @@ function saving() {
     console.log(students)
 };
 
-function createTable(){
+function createTable() {
     var container = document.getElementById('container');
     table = document.createElement('table');
+    table.id = 'table'
     tbody = document.createElement('tbody');
     container.appendChild(table);
     table.appendChild(tbody);
     this.createNameFields();
 };
 
-function createNameFields(){
+function createNameFields() {
     var nameRow = document.createElement('tr');
-    for (var item in new Student){
+    for (var item in new Student) {
         var nameCell = document.createElement('th');
         nameCell.textContent = item.toUpperCase();
         nameRow.appendChild(nameCell);
     }
-    tbody.appendChild(nameRow); 
+    tbody.appendChild(nameRow);
 };
 
-function createRowContent(student){
+function createRowContent(student) {
     var row = document.createElement('tr');
-    for (var item in student){
+    for (var item in student) {
         var cell = document.createElement('td');
         cell.textContent = student[item];
         row.appendChild(cell);
@@ -54,17 +55,30 @@ function createRowContent(student){
     tbody.appendChild(row);
 };
 
-function createDeleteButton(){
+function createDeleteButton() {
     var cellDelete = document.createElement('td');
     var button = document.createElement('button');
     button.type = 'button';
     button.innerText = ' X ';
-    button.setAttribute('onclick', "deleteStudent("+1+");")
+    /* Call method with params:
+        button.setAttribute('onclick', "deleteStudent(" + 1 + ");")
+    */
+    button.setAttribute('onclick', "captureIndexRow();")
     cellDelete.appendChild(button);
     return cellDelete;
 };
 
-function deleteStudent(position){
-    table.deleteRow(position);
-    students.pop(position);
+function captureIndexRow() {
+    var tableCopy = document.getElementById('table'), rIndex;
+    for (var i = 0; i < tableCopy.rows.length; i++) {
+        for (var j = 0; j < table.rows[i].cells.length; j++) {
+            tableCopy.rows[i].cells[j].onclick = function () {
+                rIndex = this.parentElement.rowIndex;
+                console.log(rIndex);
+                table.deleteRow(rIndex);
+                students.splice(rIndex-1, 1);
+            }
+        }
+    }
+    console.log(students);
 };
